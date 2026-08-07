8fe654793c3041b31e8488b9a9be18e2086a5246
muhammedalakbarli <m.alakbarli2007@gmail.com>
2026-08-07 08:48:58 +0400

    İngilis dili 7 tam fənn: Past tenses, Past Perfect, Passive, First Conditional, Modal verbs, Reported speech+lüğət (6 bölmə)

--- lib/content/index.ts
+++ lib/content/index.ts
@@ -10,13 +10,14 @@ import { azerbaijani6 } from "./azerbaijani6";
 import { english6 } from "./english6";
 import { math7 } from "./math7";
 import { azerbaijani7 } from "./azerbaijani7";
+import { english7 } from "./english7";
 
 // 5-ci sinif fənləri + 6-cı sinif fənləri. App dashboard-da istifadəçinin
 // sinfinə (user_metadata.grade) görə süzür (bax lib/grade.ts).
 export const subjects: Subject[] = [
   math, azerbaijani, english,
   math6, azerbaijani6, english6,
-  math7, azerbaijani7,
+  math7, azerbaijani7, english7,
 ];
 
 export function getSubject(slug: string): Subject | undefined {
